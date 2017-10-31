1403793469b077a739dd7bcabc2c8973408a9887
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-10-31 11:36:35 +0100

    Add query for finding valid stops with geojson and write to csv file

---
+++ valid_stops_with_geojson/valid_stops.sql
@@ -0,0 +1,22 @@
+copy (SELECT s.netex_id,
+          s.name_value,
+          s.stop_place_type,
+          ST_AsGeoJSON(s.centroid, 6)
+   FROM stop_place s
+   LEFT JOIN stop_place p ON p.netex_id = s.parent_site_ref
+   AND cast(p.version AS text) = s.parent_site_ref_version
+   WHERE
+     s.parent_stop_place = FALSE
+     AND ((p.netex_id IS NOT NULL
+           AND (p.from_date IS NULL
+                OR p.from_date <= '2017-11-02 01:00:00')
+           AND (p.to_date IS NULL
+                OR p.to_date > '2017-11-02 01:00:00'))
+          OR (p.netex_id IS NULL
+              AND (s.from_date IS NULL
+                   OR s.from_date <= '2017-11-02 01:00:00')
+              AND (s.to_date IS NULL
+                   OR s.to_date > '2017-11-02 01:00:00')))
+   ORDER BY s.netex_id,
+            s.version
+) TO '/tmp/test.csv' DELIMITER ';' CSV;
