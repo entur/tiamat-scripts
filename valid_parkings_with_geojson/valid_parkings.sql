3bda02a81b9ee5bae60830e99ae3b50e95f4c84c
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-11-29 14:50:38 +0100

    Update query to fetch only car parks (park and ride)

--- valid_parkings_with_geojson/valid_parkings.sql
+++ valid_parkings_with_geojson/valid_parkings.sql
@@ -3,7 +3,9 @@ COPY
           p.name_value,
           ST_AsGeoJSON(p.centroid, 6)
    FROM parking p
-   WHERE (p.from_date IS NULL
+   INNER JOIN parking_parking_vehicle_types pt ON p.id = pt.parking_id
+   WHERE pt.parking_vehicle_types = 'CAR'
+     AND (p.from_date IS NULL
           OR p.from_date <= '2017-11-28 01:00:00')
      AND (p.to_date IS NULL
           OR p.to_date > '2017-11-28 01:00:00')
