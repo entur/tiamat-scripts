9635200d37a378b756e989e917bc183f3b89f068
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-01-04 14:23:03 +0100

    NRP-2439 Update rail station names

---
+++ NRP-2439_update_rail_station_names/rename.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cat rename_rail_station_names.csv | while read line; do
+  id=$(echo $line | cut -d';' -f1)
+  newname=$(echo $line | cut -d';' -f3)
+  echo "update stop_place s set name_value='$newname' where s.netex_id = '$id' and s.version = (select max(sv.version) from stop_place sv where sv.netex_id = s.netex_id);"
+done
