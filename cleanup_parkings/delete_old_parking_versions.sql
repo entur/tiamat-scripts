ec2d8771b5248d6fc8c5d5ea6398cd9b0be02dce
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-01-08 16:21:17 +0100

    Put all sql commands in one script to avoid executing in incorrect order

--- cleanup_parkings/delete_old_parking_versions.sql
+++ cleanup_parkings/delete_old_parking_versions.sql
@@ -1,3 +1,9 @@
 delete from parking_key_values where parking_id in (select p1.id from parking p1 where p1.version < (select max(p2.version) from parking p2 where p2.netex_id = p1.netex_id));
 delete from parking_parking_vehicle_types where parking_id in (select p1.id from parking p1 where p1.version < (select max(p2.version) from parking p2 where p2.netex_id = p1.netex_id));
 delete from parking p1 where p1.version < (select max(p2.version) from parking p2 where p2.netex_id = p1.netex_id);
+
+delete from parking_key_values where parking_id in (select p1.id from parking p1 where p1.to_date < now());
+delete from parking_parking_vehicle_types where parking_id in (select p1.id from parking p1 where p1.to_date < now());
+delete from parking where to_date < now();
+
+update parking set from_date = created where from_date is null;
--- cleanup_parkings/update_parking_from_date.sql
+++
@@ -1,1 +0,0 @@
-update parking set from_date = created where from_date is null;
