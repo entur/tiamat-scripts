d6b87972ccec342b52fbe881af9cbec8983b9be6
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-01-18 15:01:18 +0100

    Fetch topographic places from quays. Fetch max version of quays. Update result

--- stops_with_description/quays-with-description.sql
+++ stops_with_description/quays-with-description.sql
@@ -1,24 +1,18 @@
 COPY
-  ( SELECT s.netex_id,
-           q.netex_id,
+  ( SELECT q.netex_id,
            q.description_value,
-           ST_AsGeoJSON(q.centroid, 6) stopCoordinate
-   FROM stop_place s
-   LEFT JOIN stop_place p ON p.netex_id = s.parent_site_ref
-   INNER JOIN stop_place_quays spq ON spq.stop_place_id = s.id
-   INNER JOIN quay q ON q.id = spq.quays_id
-   AND cast(p.version AS text) = s.parent_site_ref_version
-   WHERE ((p.netex_id IS NOT NULL
-           AND (p.from_date IS NULL
-                OR p.from_date <= now())
-           AND (p.to_date IS NULL
-                OR p.to_date > now()))
-          OR (p.netex_id IS NULL
-              AND (s.from_date IS NULL
-                   OR s.from_date <= now())
-              AND ( s.to_date IS NULL
-                   OR s.to_date > now()) ) )
+           tp.name_value,
+           ST_AsGeoJSON(q.centroid, 6)
+   FROM quay q
+   INNER JOIN stop_place_quays spq
+    ON spq.quays_id = q.id
+   INNER JOIN stop_place s
+    ON s.id = spq.stop_place_id
+    INNER JOIN topographic_place tp
+     on tp.id = s.topographic_place_id
+   WHERE
+    q.version = (select max(qv.version) from quay qv where qv.netex_id = q.netex_id)
      AND q.description_value <> ''
-   ORDER BY s.netex_id,
-            s.version) TO STDOUT
+   ORDER BY q.netex_id,
+            q.version) TO STDOUT
 DELIMITER ';' CSV;
