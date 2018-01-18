fb8e85545408307eb846f7a9a3180e5b8ee3e590
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-01-18 13:16:54 +0100

    Add coordinates

--- stops_with_description/stops-with-dscription.sql
+++ stops_with_description/stops-with-dscription.sql
@@ -3,7 +3,8 @@ COPY
            s.description_value,
            p.description_value,
            s.stop_place_type,
-           s.parent_site_ref
+           s.parent_site_ref,
+           ST_AsGeoJSON(s.centroid, 6) stopCoordinate
    FROM stop_place s
    LEFT JOIN stop_place p ON p.netex_id = s.parent_site_ref
    AND cast(p.version AS text) = s.parent_site_ref_version
@@ -17,7 +18,7 @@ COPY
                    OR s.from_date <= now())
               AND ( s.to_date IS NULL
                    OR s.to_date > now()) ) )
-     AND coalesce(s.description_value, p.description_value) <> ''
+     AND s.description_value <> ''
    ORDER BY s.netex_id,
             s.version) TO STDOUT
 DELIMITER ';' CSV;
