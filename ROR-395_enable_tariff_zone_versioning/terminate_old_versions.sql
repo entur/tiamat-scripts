f507ab5de3a6864af8423d167d67d31a4e4379cc
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-06-15 12:57:11 +0200

    ROR-395 Add sql script for adding to_date for tariff zones when there are multiple versions

---
+++ ROR-395_enable_tariff_zone_versioning/terminate_old_versions.sql
@@ -0,0 +1,13 @@
+UPDATE tariff_zone
+SET to_date=now(), version_comment = 'ROR-395 Terminating old versions manually'
+WHERE id IN
+(
+    SELECT tz.id
+    FROM tariff_zone tz
+    WHERE tz.version < (
+        SELECT MAX(tz2.version)
+        FROM tariff_zone tz2
+        WHERE tz2.netex_id = tz.netex_id
+    )
+    AND tz.netex_id = 'NTR:TariffZone:731'   
+)
