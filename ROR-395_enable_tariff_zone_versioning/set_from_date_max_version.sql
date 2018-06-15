579d7da26a89cd17f9603efbb8be846219e1ebdf
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-06-15 13:16:26 +0200

    ROR-395 Add script for setting from date for max version. Remove version_comment as it could be confusing

--- ROR-395_enable_tariff_zone_versioning/set_from_date_max_version.sql
+++ ROR-395_enable_tariff_zone_versioning/set_from_date_max_version.sql
@@ -1,10 +1,10 @@
 UPDATE tariff_zone
-SET to_date=now() - interval '1 day'
+SET from_date=now() - interval '1 day', changed=now()
 WHERE id IN
 (
     SELECT tz.id
     FROM tariff_zone tz
-    WHERE tz.version < (
+    WHERE tz.version = (
         SELECT MAX(tz2.version)
         FROM tariff_zone tz2
         WHERE tz2.netex_id = tz.netex_id
